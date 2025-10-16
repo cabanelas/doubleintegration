32c806d19b6d78184022426dfb95883397cd8309
Alexandra Cabanelas <acabanelas@whoi.edu>
2025-10-16 12:49:06 -0400

    cleaned up script added some minor notes

--- ARcoef_PDOdriver_CCE.R
+++ ARcoef_PDOdriver_CCE.R
@@ -15,6 +15,8 @@
 ## STEP 1 of Monte Carlo analysis
 # Step 1 - estimate autoregression coeff from the original data/signals to be
 #able to use for creating two red-noise time series 
+# the goal is to use the AR1 coefficient to create red-noise surrogates that mimic
+# the autocorreltion structure of the original PDO 
 
 ## ------------------------------------------ ##
 #            Packages -----
@@ -26,32 +28,29 @@ library(tidyverse)
 #library(urca) #v1.3.3
 
 ## ------------------------------------------ ##
-#            DATA -----
+#            DATA & TIDY -----
 ## ------------------------------------------ ##
-PDO <- read.csv(file.path("raw", "CCE",
-                          "PDO.csv"))
+PDO <- read.csv(file.path("raw", 
+                          "CCE",
+                          "PDO.csv")) %>% # contains up to Dec 2024
+       # long format
+       pivot_longer(cols = Jan:Dec, 
+                    names_to = "month", 
+                    values_to = "pdo") %>%
+       as.data.frame() %>%
+       mutate(
+         # fix date
+         Date = as.Date(paste(Year, month, "01"),
+                        format = "%Y %b %d"),
+         #turn month into integer
+         month = match(month, month.abb)
+       ) %>%
+       arrange(Date) %>%
+       #filtering 10 yrs before bio data
+       filter(Year >= 1941, Year <= 2021, pdo < 99) 
 
-## ------------------------------------------ ##
-#            TIDY -----
-## ------------------------------------------ ##
-# long format
-PDO <-  PDO %>%
-  pivot_longer(cols = Jan:Dec,
-               names_to = "month",
-               values_to = "pdo") %>%
-  as.data.frame()
-
-# fix date
-PDO$Date <- as.Date(paste(PDO$Year, PDO$month, "01"), 
-                    format = "%Y %b %d")
-
-PDO$month <- match(PDO$month, month.abb)
-
-PDO <- PDO %>% 
-  filter(pdo < 99 & Year > 1940) #filtering 10 yrs before bio data
-
-# create time series object
-PDOts <- ts(PDO$pdo, start = c(1940, 1), frequency = 12)
+# --- create time series object
+PDOts <- ts(PDO$pdo, start = c(1941, 1), frequency = 12)
 
 head(PDO)
 head(PDOts)
@@ -62,8 +61,8 @@ head(PDOts)
 # A time series that is non-stationary (has a trend or changing variance) 
 #should be detrended before AR(1) estimation
 
-## No need to detrend Multivariate ENSO Index TS
-## first, visual check...
+## No need to detrend PDO Index TS
+## visual check...
 plot(PDOts, main = "Pacific Decadal Oscillation PDO Index", 
      ylab = "PDO", 
      xlab = "Year", 
@@ -71,13 +70,15 @@ plot(PDOts, main = "Pacific Decadal Oscillation PDO Index",
      col = "black", 
      lwd = 2)
 # no obvious upward or downward slope 
+# PDO is already an anomaly index so unlikely that detrending is needed
 
 PDO_decomp <- decompose(PDOts, type = "additive")  # use "multiplicative" if variance increases over time
 plot(PDO_decomp)
 
 # to confirm, can perform Augmented Dickey-Fuller test 
-# ADF pval = 0.01 == no need to detrend (if pvalue > 0.5 ts has trend)
-adf.test(PDOts) #as we know these tests can be a bit misleading
+# ADF pval = 0.01 == no need to detrend
+#if pvalue > 0.05 ts has trend; fail to reject unit root
+adf.test(PDOts) #these tests can be a bit misleading
 acf(PDOts)
 # double checked anyways and detrending vs not detrending == same AR1 coef
 
@@ -92,13 +93,13 @@ acf(PDOts)
 AR1_model <- Arima(PDOts, order = c(1,0,0))
 
 # print summary
-print(summary(AR1_model))
-ar1_coefficient <- AR1_model$coef["ar1"]  
+summary(AR1_model)
+ar1_coefficient <- AR1_model$coef["ar1"] #or coef(AR1_model)[["ar1"]]
 ar1_coefficient
 ## ------------------------------------------ ##
 # diagnostic plots
 ## ------------------------------------------ ##
-tsdiag(AR1_model)
+tsdiag(AR1_model) #forecast::checkresiduals(AR1_model)
 
 # extract residuals
 PDO_residuals <- residuals(AR1_model)
@@ -106,6 +107,7 @@ PDO_residuals <- residuals(AR1_model)
 # plot original time series with fitted values
 par(mfrow = c(2, 2))
 
+# ts (top left)
 ts.plot(PDOts, main = "PDO Time Series with AR(1) Fit", ylab = "PDO")
 lines(fitted(AR1_model), col = "red", lwd = 2)
 
@@ -128,9 +130,10 @@ par(mfrow = c(1, 1))
 ## ------------------------------------------ ##
 # 3) Export AR coefficient
 ## ------------------------------------------ ##
-se <- sqrt(diag(vcov(AR1_model)))[1]  # standard error
+se <- sqrt(diag(vcov(AR1_model)))[1]  # standard error. or  sqrt(vcov(AR1_model)["ar1","ar1"])
 sigma2 <- AR1_model$sigma2
-
+# sd <- sqrt(sigma2)
+# ci <- ar1_coefficient + c(-1.96, 1.96) * se
 ar_info_df <- data.frame(ts_name = "PDO",
                          AR_coef = ar1_coefficient,
                          se = se,
